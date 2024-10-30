bd2a9c6a63c9d1ee641861cba2047dcef009702f
jaykumar kota <kota.jaykumar@gamil.com>
2024-10-30 23:39:08 +0530

    version4

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let hamIcon = document.querySelector("#ham-icon");
 let navLinks = document.querySelector(".nav-links");
+let crossIcon = document.querySelector("#cross-icon");
 let isSideMenuOpen = false;
 hamIcon.addEventListener("click", () => {
   if (!isSideMenuOpen) {
-    navLinks.style.right = "0";
+    navLinks.style.display = "block";
+    hamIcon.style.display = "none";
+    crossIcon.style.display = "block";
     isSideMenuOpen = true;
-  } else {
-    navLinks.style.right = "-200px";
+  }
+});
+crossIcon.addEventListener("click", () => {
+  if (isSideMenuOpen) {
+    navLinks.style.display = "none";
+    hamIcon.style.display = "block";
+    crossIcon.style.display = "none";
     isSideMenuOpen = false;
   }
 });
